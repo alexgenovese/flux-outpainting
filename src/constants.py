e363cc3bacfea8d928a43de241ee3ce3ea948acf
Alex <423745+alexgenovese@users.noreply.github.com>
2024-11-27 11:50:31 +0100

    mv weights cache variable

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,21 @@
 import os 
+base_path = os.path.join(os.path.abspath(os.getcwd()), 'weights-cache')
 
-base_path = os.path.abspath(os.getcwd())
+os.environ["HF_DATASETS_OFFLINE"] = "1"
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
+os.environ["HF_HOME"] = base_path
+os.environ["TORCH_HOME"] = base_path
+os.environ["HF_DATASETS_CACHE"] = base_path
+os.environ["TRANSFORMERS_CACHE"] = base_path
+os.environ["HUGGINGFACE_HUB_CACHE"] = base_path
+
+
 BASE_MODEL = "black-forest-labs/FLUX.1-dev"
-BASE_MODEL_CACHE = os.path.join( base_path, 'weights-cache/base_model')
+BASE_MODEL_CACHE = os.path.join( base_path, 'base_model')
 CONTROLNET_MODEL = "alimama-creative/FLUX.1-dev-Controlnet-Inpainting-Beta"
-CONTROLNET_MODEL_CACHE = os.path.join( base_path, 'weights-cache/controlnet-inpainting-beta' ) 
+CONTROLNET_MODEL_CACHE = os.path.join( base_path, 'controlnet-inpainting-beta' ) 
 CONTROL_LOCAL = ""
-UPSCALER_CACHE = os.path.join( base_path, 'weights-cache/upscaler_model')
+UPSCALER_CACHE = os.path.join( base_path, 'upscaler_model')
 UPSCALER_MODEL = ""
 
 hf_token = "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"
